4489342ada3034572ea35dc890f7710fe2405a03
fhidesjoseph <fhidesjoseph@gmail.com>
2022-06-10 00:23:40 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // code your solution here
+const saturdayFun = function(event='roller-skate'){
+    return `This Saturday, I want to ${event}!`;
+}
+const mondayWork = function(event='go to the office'){
+    return `This Monday, I will ${event}.`;
+}
+const wrapAdjective=function(event=`*`){
+    return function (type = 'a dedicated programmer'){
+        return `You are ${event}${type}${event}!`
+}
+}
